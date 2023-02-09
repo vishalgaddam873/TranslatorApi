cd176dd684941757bacbac5bae105a920d0c2b02
Vishal Gaddam <41083456+vishalgaddam873@users.noreply.github.com>
2023-02-09 11:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def welcome():
     
     return jsonify(phrase= gtranslation.text , pr= p.pronunciation)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+if __name__ == "__main__":
+    app.run(debug=True)
